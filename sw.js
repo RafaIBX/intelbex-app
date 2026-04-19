0705181325c9707289ccd4da626eadb81e2b1fde
RafaIBX <rafael@intelbex.com>
2026-04-19 16:28:36 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,52 +1,23 @@
-const CACHE_NAME = 'intelbex-v1';
+const CACHE_NAME = 'intelbex-v2';
 
 const SHELL = [
-  './',
-  './index.html',
-  './manifest.json',
-  './content/config.json',
+  '/intelbex-app/',
+  '/intelbex-app/index.html',
+  '/intelbex-app/manifest.json',
+  '/intelbex-app/content/config.json',
 ];
 
 self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) =>
-      cache.addAll(SHELL).catch(() => {})
-    )
-  );
+  e.waitUntil(caches.open(CACHE_NAME).then((c) => c.addAll(SHELL).catch(() => {})));
   self.skipWaiting();
 });
 
 self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
-    )
-  );
+  e.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))));
   self.clients.claim();
 });
 
 self.addEventListener('fetch', (e) => {
-  const { request } = e;
-  if (request.method !== 'GET') return;
-
-  if (request.url.includes('/content/config.json')) {
-    e.respondWith(
-      fetch(request).then((res) => {
-        const clone = res.clone();
-        caches.open(CACHE_NAME).then((c) => c.put(request, clone));
-        return res;
-      }).catch(() => caches.match(request))
-    );
-    return;
-  }
-
-  e.respondWith(
-    caches.match(request).then((hit) => hit || fetch(request).then((res) => {
-      if (res && res.status === 200 && res.type === 'basic') {
-        const clone = res.clone();
-        caches.open(CACHE_NAME).then((c) => c.put(request, clone));
-      }
-      return res;
-    }))
-  );
+  if (e.request.method !== 'GET') return;
+  e.respondWith(caches.match(e.request).then((hit) => hit || fetch(e.request)));
 });
